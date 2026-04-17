a503228ff8e85f19ef19d8d81cf0579fc3ae0b24
Victor Gois <victorgois18@gmail.com>
2026-04-17 14:42:20 -0300

    Export charts and shared funcs

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
+// Components
 export { default as BarChart } from './components/BarChart.svelte';
 export { default as LineChart } from './components/LineChart.svelte';
 export { default as AgeGroupChart } from './components/AgeGroupChart.svelte';
@@ -6,4 +7,21 @@ export { default as ChoroplethMap } from './components/ChoroplethMap.svelte';
 export { default as GenderDivergingBar } from './components/GenderDivergingBar.svelte';
 export { default as StackedBarChart } from './components/StackedBarChart.svelte';
 export { default as TierSmallMultiples } from './components/TierSmallMultiples.svelte';
+
+// Tokens
 export * from './tokens.js';
+
+// Shared utilities
+export * from './utils/formatters.js';
+export * from './utils/tooltip.js';
+export * from './utils/geoLoader.js';
+export * from './utils/axisHelpers.js';
+export * from './utils/resizeObserver.js';
+
+// Chart draw functions (for headless / custom-shell usage)
+export * from './charts/ageGroupChart.js';
+export * from './charts/bubbleScatter.js';
+export * from './charts/genderDivergingBar.js';
+export * from './charts/choroplethMap.js';
+export * from './charts/stackedBarChart.js';
+export * from './charts/tierSmallMultiples.js';
