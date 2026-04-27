b3fa677fc8e169f8bd74801f397991e3e484f1cc
Victor Gois <victorgois18@gmail.com>
2026-04-27 15:24:07 -0300

    Update config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    dts({ include: ['src/lib'], insertTypesEntry: true }),
+  ],
   build: {
     lib: {
       entry: 'src/lib/index.ts',
       name: 'sniic',
       fileName: 'sniic',
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
-      external: ['svelte', 'd3', /^d3-/],
+      external: ['svelte', /^svelte\//, 'd3', /^d3-/],
       output: {
-        globals: { svelte: 'svelte' },
+        globals: { svelte: 'svelte', 'svelte/internal/client': '$', d3: 'd3' },
       },
     },
   },
