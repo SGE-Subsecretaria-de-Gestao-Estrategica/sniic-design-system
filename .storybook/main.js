54ac5f4b781bd5bf376f02a534afa43ea618d82d
Italo Doliva <italocoltact@gmail.com>
2026-07-23 12:17:36 -0300

    improve: Storybook is now considering path aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
+import { mergeConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-/** @type { import('@storybook/svelte-vite').StorybookConfig } */
 const config = {
   "stories": [
     "../src/**/*.mdx",
@@ -13,6 +16,15 @@ const config = {
     "@storybook/addon-a11y",
     "@storybook/addon-docs"
   ],
-  "framework": "@storybook/svelte-vite"
+  "framework": "@storybook/svelte-vite",
+  async viteFinal(viteConfig) {
+    return mergeConfig(viteConfig, {
+      resolve: {
+        alias: {
+          $lib: path.resolve(__dirname, '../src/lib'),
+        },
+      },
+    });
+  },
 };
 export default config;
